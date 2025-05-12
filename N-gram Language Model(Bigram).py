752f532beb784d31e81eff4dcc1b95827f52c349
Raasikh Ahmed Naveed <101375636+Raasikh@users.noreply.github.com>
2025-05-12 15:35:53 -0500

    Add files via upload

---
+++ N-gram Language Model(Bigram).py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+words = open('names.txt', 'r').read().splitlines()
+words[:10]
+
+
+# In[2]:
+
+
+b = {}
+for w in words:
+  chs = ['<S>'] + list(w) + ['<E>']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    bigram = (ch1, ch2)
+    b[bigram] = b.get(bigram, 0) + 1
+
+
+# In[3]:
+
+
+sorted(b.items(), key = lambda kv: -kv[1])
+
+
+# In[ ]:
+
+
+import torch
+
+N = torch.zeros((27, 27), dtype=torch.int32)
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
+
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    N[ix1, ix2] += 1
+    
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+        plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
+plt.axis('off');
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# GOAL: maximize likelihood of the data w.r.t. model parameters (statistical modeling)
+# equivalent to maximizing the log likelihood (because log is monotonic)
+# equivalent to minimizing the negative log likelihood
+# equivalent to minimizing the average negative log likelihood
+
+# log(a*b*c) = log(a) + log(b) + log(c)
+
+
+# In[ ]:
+
+
+log_likelihood = 0.0
+n = 0
+
+for w in words:
+#for w in ["andrejq"]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    prob = P[ix1, ix2]
+    logprob = torch.log(prob)
+    log_likelihood += logprob
+    n += 1
+    #print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+print(f'{log_likelihood=}')
+nll = -log_likelihood
+print(f'{nll=}')
+print(f'{nll/n}')
+
+
+# In[ ]:
+
+
+# create the training set of bigrams (x,y)
+xs, ys = [], []
+
+for w in words[:1]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    print(ch1, ch2)
+    xs.append(ix1)
+    ys.append(ix2)
+    
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+
+# In[ ]:
+
+
+import torch.nn.functional as F
+xenc = F.one_hot(xs, num_classes=27).float()
+xenc
+
+
+# In[ ]:
+
+
+W = torch.randn((27, 1))
+xenc @ W
+
+
+# In[ ]:
+
+
+logits = xenc @ W # log-counts
+counts = logits.exp() # equivalent N
+probs = counts / counts.sum(1, keepdims=True)
+loss = -probs[torch.arange(5), ys].log().mean()
+
+
+# In[ ]:
+
+
+# create the dataset
+xs, ys = [], []
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    xs.append(ix1)
+    ys.append(ix2)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples: ', num)
+
+# initialize the 'network'
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+
+# In[ ]:
+
+
+# gradient descent
+for k in range(1):
+  
+  # forward pass
+  xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+  logits = xenc @ W # predict log-counts
+  counts = logits.exp() # counts, equivalent to N
+  probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+  loss = -probs[torch.arange(num), ys].log().mean() + 0.01*(W**2).mean()
+  print(loss.item())
+  
+  # backward pass
+  W.grad = None # set to zero the gradient
+  loss.backward()
+  
+
+
+# In[ ]:
+
+
+# update
+W.data += -50 * W.grad
+
+
+# In[ ]:
+
+
+# finally, sample from the 'neural net' model
+g = torch.Generator().manual_seed(2147483647)
+
+for i in range(5):
+  
+  out = []
+  ix = 0
+  while True:
+    
+    # ----------
+    # BEFORE:
+    #p = P[ix]
+    # ----------
+    # NOW:
+    xenc = F.one_hot(torch.tensor([ix]), num_classes=27).float()
+    logits = xenc @ W # predict log-counts
+    counts = logits.exp() # counts, equivalent to N
+    p = counts / counts.sum(1, keepdims=True) # probabilities for next character
+    # ----------
+    
+    ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+    out.append(itos[ix])
+    if ix == 0:
+      break
+  print(''.join(out))
+
